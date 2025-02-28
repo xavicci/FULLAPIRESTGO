266e36844d39adddca3d72a52076eba818758c94
Xavi Flores <xa_franc@live.com>
2025-02-28 09:18:19 -0600

    add handlers and routes

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/xavicci/FULLAPIRESTGO/handlers"
+	"github.com/xavicci/FULLAPIRESTGO/server"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	PORT := os.Getenv("PORT")
+	JWT_SECRET := os.Getenv("JWT_SECRET")
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+
+	s, err := server.NewServer(context.Background(), &server.Config{
+		JWTSecret:   JWT_SECRET,
+		Port:        PORT,
+		DatabaseUrl: DATABASE_URL,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.Start(BindRoutes)
+}
+
+func BindRoutes(s server.Server, r *mux.Router) {
+	r.HandleFunc("/", handlers.HomeHandler(s)).Methods(http.MethodGet)
+}
--- server/server.go
+++ server/server.go
@@ -30,13 +30,13 @@ func (b *Broker) Config() *Config {
 
 func NewServer(ctx context.Context, config *Config) (*Broker, error) {
 	if config.Port == "" {
-		return nil, errors.New("Port is required")
+		return nil, errors.New("port is required")
 	}
 	if config.JWTSecret == "" {
-		return nil, errors.New("Secret is required")
+		return nil, errors.New("secret is required")
 	}
 	if config.DatabaseUrl == "" {
-		return nil, errors.New("Database URL is required")
+		return nil, errors.New("database URL is required")
 	}
 
 	broker := &Broker{
@@ -50,7 +50,7 @@ func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
 	b.router = mux.NewRouter()
 	binder(b, b.router)
 	log.Println("Starting server on port", b.Config().Port)
-	if err := http.ListenAndServe(b.config.Port, b.router); err != nil {
+	if err := http.ListenAndServe(":"+b.config.Port, b.router); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
