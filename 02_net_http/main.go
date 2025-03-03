a6190b4db30bbf097b96c1c52c906e0e44c42773
Xavi Flores <xa_franc@live.com>
2025-03-02 21:48:31 -0600

    create handlers http

--- 02_net_http/main.go
+++ 02_net_http/main.go
@@ -4,16 +4,24 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	domain "github.com/xavicci/FULLAPIRESTGO/domain"
 )
 
 func main() {
 	http.HandleFunc("create-user", createUserHandler)
+	http.HandleFunc("get-user", getUserHandler)
 	fmt.Println("Starting server on port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
+}
+
+func getUserHandler(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.Path, "/")
+	id := parts[len(parts)-1]
+	fmt.Fprintf(w, "Request user id: %s", id)
 }
 
 func createUserHandler(w http.ResponseWriter, r *http.Request) {
