0cea02cbf889c4cb1bc4b83a874d5a7849533540
Xavi Flores <xa_franc@live.com>
2025-03-15 13:01:22 -0600

    end

--- handlers/post.go
+++ handlers/post.go
@@ -157,5 +157,12 @@ func ListPostsHandler(s server.Server) http.HandlerFunc {
 				return
 			}
 		}
+		post, err := repository.ListPosts(r.Context(), page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(post)
 	}
 }
